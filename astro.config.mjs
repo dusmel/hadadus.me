b01b31f5d61f14cfbabcf7549175c10d778f1798
dusmel <hadadus@gmail.com>
2023-10-04 12:12:35 +0200

    add analytics with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
-
+import vercel from '@astrojs/vercel/serverless';
 import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss({ safelist: ['data-theme', /^data-/], })]
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  // integrations: [purgecss({ safelist: ['data-theme', /^data-/], })]
 });
